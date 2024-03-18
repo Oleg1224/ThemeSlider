a8fe65afbf4b821a7ab2c458307085a0ad4d4721
Oleg1224 <olegik.kr@gmail.com>
2024-03-18 16:08:41 +0200

    1

--- script.js
+++ script.js
@@ -30,3 +30,13 @@ for(let i = 0; i<btn.length; i++) {
         toSlide(i)
     })
 }
+let headMenu = document.querySelector('.header__menu');
+let menu = document.querySelector('.menu')
+headMenu.addEventListener('click', function() {
+    if(menu.className === 'menu') {
+        menu.classList.add('new')
+    }
+    else {
+        menu.classList.remove('new')
+    }
+})
