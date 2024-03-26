77c9fa346a7c393ff18b2577db9307bcfd135c83
Oleg1224 <olegik.kr@gmail.com>
2024-03-26 21:23:15 +0200

    1

--- mobileMenu.js
+++ mobileMenu.js
@@ -3,11 +3,17 @@ let menu = document.querySelector('.menu')
 headMenu.addEventListener('click', function() {
     /* headMenu.style.transform = `rotate(360deg)` */
     if(menu.className === 'menu') {
+        headMenu.src = "./img/pngwing.comыаыаыва.png"
         menu.classList.add('new')
         headMenu.classList.add('rotate')
     }
     else {
         menu.classList.remove('new')
         headMenu.classList.remove('rotate')
+        headMenu.src = "./img/pngwing.com (13).png"
     }
+    /* menu.style.display = 'block'; */
+    /* setTimeout(function() {
+        menu.style.borderRadius = '0%'; // Делаем круглый див прямоугольным
+    }, 10); */
 })
